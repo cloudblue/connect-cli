171aebdff39751241351ff752a2307d97b368ea9
Francesco Faraone <francesco.faraone@ingrammicro.com>
2020-09-09 11:11:29 +0200

    fix version gathering

--- cnctcli/__init__.py
+++ cnctcli/__init__.py
@@ -1,9 +1,9 @@
-from importlib.metadata import version, PackageNotFoundError
+from setuptools_scm import get_version as scm_version
 
 try:
-    __version__ = version(__name__)
-except PackageNotFoundError:
-    __version__ = '0.0.0'
+    __version__ = scm_version(root='..', relative_to=__file__)
+except:   # noqa: E722
+    __version__ = '0.0.1'
 
 
 def get_version():
