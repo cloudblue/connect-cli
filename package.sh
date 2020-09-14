30b667e9b2006b153098a83c85b8d8bc5e4d2833
Francesco Faraone <francesco.faraone@ingrammicro.com>
2020-09-14 10:27:07 +0200

    deploy to github releases

---
+++ package.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Run pyinstaller..."
+
+pyinstaller resources/ccli.spec
+
+echo "Create tarball for $TRAVIS_OS_NAME -> $TRAVIS_CPU_ARCH..."
+
+tar cvfz product-sync_$TRAVIS_TAG_$TRAVIS_OS_NAME_$TRAVIS_CPU_ARCH.tar.gz dist
+rm -rf dist
+
+echo "Done!"
