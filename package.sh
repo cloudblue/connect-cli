7b862410cf69c832b2ad93ec92a009cbb6ebb062
Francesco Faraone <francesco.faraone@ingrammicro.com>
2020-09-14 12:28:03 +0200

    package with zip on windows

--- package.sh
+++ package.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
+set -e
+
+
+
 echo "Run pyinstaller..."
 
 pyinstaller resources/ccli.spec
 
 echo "Create tarball for $TRAVIS_OS_NAME -> $TRAVIS_CPU_ARCH..."
 
-tar cvfz product-sync_${TRAVIS_TAG}_${TRAVIS_OS_NAME}_${TRAVIS_CPU_ARCH}.tar.gz dist
+if [ "$TRAVIS_OS_NAME" == "windows" ]; then
+    tar.exe -a -c -f product-sync_${TRAVIS_TAG}_${TRAVIS_OS_NAME}_${TRAVIS_CPU_ARCH}.zip dist
+else
+    tar cvfz product-sync_${TRAVIS_TAG}_${TRAVIS_OS_NAME}_${TRAVIS_CPU_ARCH}.tar.gz dist
+fi
+
+
 rm -rf dist
 
 echo "product-sync_${TRAVIS_TAG}_${TRAVIS_OS_NAME}_${TRAVIS_CPU_ARCH}.tar.gz ready!"
