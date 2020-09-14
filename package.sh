f34270246a4bcd694eb0f6fa1b04146758e754ce
Francesco Faraone <francesco.faraone@ingrammicro.com>
2020-09-14 12:37:18 +0200

    fix for win

--- package.sh
+++ package.sh
@@ -12,11 +12,13 @@ echo "Create tarball for $TRAVIS_OS_NAME -> $TRAVIS_CPU_ARCH..."
 
 if [ "$TRAVIS_OS_NAME" == "windows" ]; then
     tar.exe -a -c -f product-sync_${TRAVIS_TAG}_${TRAVIS_OS_NAME}_${TRAVIS_CPU_ARCH}.zip dist
+    ARCH_EXT="zip"
 else
     tar cvfz product-sync_${TRAVIS_TAG}_${TRAVIS_OS_NAME}_${TRAVIS_CPU_ARCH}.tar.gz dist
+    ARCH_EXT="tar.gz"
 fi
 
 
 rm -rf dist
 
-echo "product-sync_${TRAVIS_TAG}_${TRAVIS_OS_NAME}_${TRAVIS_CPU_ARCH}.tar.gz ready!"
+echo "product-sync_${TRAVIS_TAG}_${TRAVIS_OS_NAME}_${TRAVIS_CPU_ARCH}.${ARCH_EXT} ready!"
