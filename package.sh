94f24df617535e9ebf67a8f49564696c41070d7e
Francesco Faraone <francesco.faraone@ingrammicro.com>
2020-09-14 11:24:28 +0200

    try python from homebrew

--- package.sh
+++ package.sh
@@ -6,7 +6,7 @@ pyinstaller resources/ccli.spec
 
 echo "Create tarball for $TRAVIS_OS_NAME -> $TRAVIS_CPU_ARCH..."
 
-tar cvfz product-sync_$TRAVIS_TAG_$TRAVIS_OS_NAME_$TRAVIS_CPU_ARCH.tar.gz dist
+tar cvfz product-sync_${TRAVIS_TAG}_${TRAVIS_OS_NAME}_${TRAVIS_CPU_ARCH}.tar.gz dist
 rm -rf dist
 
-echo "product-sync_$TRAVIS_TAG_$TRAVIS_OS_NAME_$TRAVIS_CPU_ARCH.tar.gz ready!"
+echo "product-sync_${TRAVIS_TAG}_${TRAVIS_OS_NAME}_${TRAVIS_CPU_ARCH}.tar.gz ready!"
