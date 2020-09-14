1a9f4ee095f1c77dcd5bf129ecb3403dc0f9e4b2
Francesco Faraone <francesco.faraone@ingrammicro.com>
2020-09-14 10:38:26 +0200

    fix travis

--- package.sh
+++ package.sh
@@ -9,4 +9,4 @@ echo "Create tarball for $TRAVIS_OS_NAME -> $TRAVIS_CPU_ARCH..."
 tar cvfz product-sync_$TRAVIS_TAG_$TRAVIS_OS_NAME_$TRAVIS_CPU_ARCH.tar.gz dist
 rm -rf dist
 
-echo "Done!"
+echo "product-sync_$TRAVIS_TAG_$TRAVIS_OS_NAME_$TRAVIS_CPU_ARCH.tar.gz ready!"
