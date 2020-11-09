9bb3c425623fc1c3982aca0464c5c363e686772a
Marc Serrat <Marc.SerratBote@ingrammicro.com>
2020-11-09 17:47:21 +0100

    Fixed publish workflow

--- package.sh
+++ package.sh
@@ -11,14 +11,14 @@ pyinstaller resources/ccli.spec
 echo "Create tarball for $OS_NAME -> $CPU_ARCH..."
 
 if [ "$OS_NAME" == "windows" ]; then
-    powershell.exe -nologo -noprofile -command "& { compress-archive -path '.\dist' -destinationpath '.\connect-cli_${TAG}_${OS_NAME}_${CPU_ARCH}.zip' }"
+    powershell.exe -nologo -noprofile -command "& { compress-archive -path '.\dist' -destinationpath '.\connect-cli_${PKG_TAG}_${OS_NAME}_${CPU_ARCH}.zip' }"
     ARCH_EXT="zip"
 else
-    tar cvfz connect-cli_${TAG}_${OS_NAME}_${CPU_ARCH}.tar.gz dist
+    tar cvfz connect-cli_${PKG_TAG}_${OS_NAME}_${CPU_ARCH}.tar.gz dist
     ARCH_EXT="tar.gz"
 fi
 
 
 rm -rf dist
 
-echo "connect-cli_${TAG}_${OS_NAME}_${CPU_ARCH}.${ARCH_EXT} ready!"
+echo "connect-cli_${PKG_TAG}_${OS_NAME}_${CPU_ARCH}.${ARCH_EXT} ready!"
