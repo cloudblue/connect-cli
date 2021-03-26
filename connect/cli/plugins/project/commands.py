1b78df928356cb7c2a51eac20ff2751d6056031a
jordi.peralta <jordi.peraltadolz@ingrammicro.com>
2021-03-26 16:04:10 +0100

    LITE-17948: Add bootstrap report project

---
+++ connect/cli/plugins/project/commands.py
@@ -0,0 +1,56 @@
+#  Copyright © 2021 CloudBlue. All rights reserved.
+
+import click
+
+from connect.cli.plugins.project.helpers import (
+    bootstrap_project,
+    list_projects,
+)
+
+import os
+
+
+@click.group(name='project', short_help='Manage project definitions.')
+def grp_project():
+    pass  # pragma: no cover
+
+
+@grp_project.group(
+    name='report',
+    short_help='Manage report projects category.',
+)
+def grp_project_report():
+    pass  # pragma: no cover
+
+
+@grp_project_report.command(
+    name='bootstrap',
+    short_help='Bootstrap new report project.',
+)
+@click.option(
+    '--data-dir', '-d',
+    default=os.getcwd(),
+    type=click.Path(exists=False, file_okay=False, dir_okay=True),
+    required=True,
+    help='Directory where the report project will be created.',
+)
+def cmd_bootstrap_report_project(data_dir):
+    bootstrap_project(data_dir)
+
+
+@grp_project_report.command(
+    name='list',
+    short_help='List report projects.',
+)
+@click.option(
+    '--data-dir', '-d',
+    default=os.getcwd(),
+    type=click.Path(exists=True, file_okay=False, dir_okay=True),
+    help='Working directory for report projects.',
+)
+def cmd_list_report_project(data_dir):
+    list_projects(data_dir)
+
+
+def get_group():
+    return grp_project
---
+++ tests/plugins/project/test_commands.py
@@ -0,0 +1,53 @@
+#  Copyright © 2021 CloudBlue. All rights reserved.
+
+from click.testing import CliRunner
+
+import os
+
+
+def test_bootstrap_command(fs, ccli, mocker, capsys):
+    mocked_bootstrap = mocker.patch(
+        'connect.cli.plugins.project.commands.bootstrap_project',
+        side_effect=print('project_dir'),
+    )
+    os.mkdir(f'{fs.root_path}/projects')
+    runner = CliRunner()
+    result = runner.invoke(
+        ccli,
+        [
+            'project',
+            'report',
+            'bootstrap',
+            '--data-dir',
+            f'{fs.root_path}/projects',
+        ],
+    )
+
+    mocked_bootstrap.assert_called_once_with(f'{fs.root_path}/projects')
+    captured = capsys.readouterr()
+    assert 'project_dir' in captured.out
+    assert result.exit_code == 0
+
+
+def test_list_project_command(fs, ccli, mocker, capsys):
+    mocked_list_project = mocker.patch(
+        'connect.cli.plugins.project.commands.list_projects',
+        side_effect=print('project_1'),
+    )
+    os.mkdir(f'{fs.root_path}/projects')
+    runner = CliRunner()
+    result = runner.invoke(
+        ccli,
+        [
+            'project',
+            'report',
+            'list',
+            '--data-dir',
+            f'{fs.root_path}/projects',
+        ],
+    )
+
+    assert result.exit_code == 0
+    mocked_list_project.assert_called_once_with(f'{fs.root_path}/projects')
+    captured = capsys.readouterr()
+    assert 'project_1' in captured.out
