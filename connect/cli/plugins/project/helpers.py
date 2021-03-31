0fc838f3df3cc5226a6f81489d8f12a604326658
jordi.peralta <jordi.peraltadolz@ingrammicro.com>
2021-03-31 18:21:30 +0200

    LITE-17949: Add validation command for report project

--- connect/cli/plugins/project/helpers.py
+++ connect/cli/plugins/project/helpers.py
@@ -12,6 +12,9 @@ from connect.cli.plugins.project.constants import (
 
 import glob
 import json
+import os
+import sys
+import inspect
 
 from cmr import render
 
@@ -19,6 +22,11 @@ import click
 from click import ClickException
 
 from connect.reports.datamodels import RepositoryDefinition
+from connect.reports.validator import (
+    validate,
+    validate_with_schema,
+)
+from connect.reports.parser import parse
 
 
 def bootstrap_project(data_dir: str):
@@ -59,3 +67,63 @@ def list_projects(data_dir: str):
         project_info.append(f'| {project.name} | {project.version} |\n')
 
     click.echo(render(''.join(project_info)))
+
+
+def validate_project(project_dir):
+    click.secho(f'Validating project {project_dir}...\n', fg='blue')
+
+    data = _file_descriptor_validations(project_dir)
+
+    errors = validate_with_schema(data)
+    if errors:
+        raise ClickException(f'Invalid `reports.json`: {errors}')
+
+    report_project = parse(project_dir, data)
+
+    errors = validate(report_project)
+    if errors:
+        raise ClickException(f'Invalid `reports.json`: {",".join(errors)}')
+
+    for report in report_project.reports:
+        _entrypoint_validations(project_dir, report.entrypoint, report.report_spec)
+
+    click.secho(f'Report Project {project_dir} has been successfully validated.', fg='green')
+
+
+def _file_descriptor_validations(project_dir):
+    descriptor_file = os.path.join(project_dir, 'reports.json')
+    if not os.path.isfile(descriptor_file):
+        raise ClickException(
+            f'The directory `{project_dir}` does not seem a report project directory, '
+            'the mandatory `reports.json` file descriptor is not present.',
+        )
+    try:
+        data = json.load(open(descriptor_file, 'r'))
+    except json.JSONDecodeError:
+        raise ClickException(
+            'The report project descriptor `reports.json` is not a valid json file.',
+        )
+    return data
+
+
+def _entrypoint_validations(project_dir, entrypoint, report_spec):
+    # Package validation
+    sys.path.append(os.path.join(os.getcwd(), project_dir))
+    package = entrypoint.rsplit('.', 1)[0]
+    try:
+        entrypoint_module = inspect.importlib.import_module(package)
+    except Exception as error:
+        raise ClickException(f'\nErrors detected on entrypoint module: {error}')
+
+    # Function validation
+    if report_spec == '1' and len(inspect.signature(entrypoint_module.generate).parameters) != 3:
+        raise ClickException(
+            'Wrong arguments on `generate` function. The signature must be:'
+            '\n>> def generate(client, parameters, progress_callback) <<',
+        )
+    if report_spec == '2' and len(inspect.signature(entrypoint_module.generate).parameters) != 5:
+        raise ClickException(
+            'Wrong arguments on `generate` function. The signature must be:'
+            '\n>> def generate(client=None, input_data=None, progress_callback=None, '
+            'renderer_type=None, extra_context_callback=None) <<',
+        )
