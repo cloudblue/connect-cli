611224a5ef8bfd30e1630e807cac6bf10131e342
Jonathan Rios <jonathan.rios@cloudblue.com>
2022-05-23 09:40:55 +0200

    Fix translation attributes sheet headers when export to xlsx

--- connect/cli/plugins/product/constants.py
+++ connect/cli/plugins/product/constants.py
@@ -109,14 +109,6 @@ TRANSLATION_HEADERS = {
     'M': 'Updated',
 }
 
-TRN_ATTR_HEADER = {
-    'A': 'Key',
-    'B': 'Original Value',
-    'C': 'Action',
-    'D': 'Value',
-    'E': 'Comment',
-}
-
 PARAM_TYPES = [
     'email',
     'address',
