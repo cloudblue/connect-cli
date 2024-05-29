aa31b60bff4da46dbbbe7a75c59bf32368ba1fb6
Mike Salnikov <mikhail.salnikov@ingrammicro.com>
2024-05-29 18:30:14 +0200

    LITE-30249 black formatting

--- tests/plugins/product/test_export.py
+++ tests/plugins/product/test_export.py
@@ -4,10 +4,12 @@ from connect.cli.plugins.product.export import _primary_translation_str
 
 
 def test_primary_translation_str_ok():
-    translation = {'locale': {
-        'id': 'jp',
-        'name': 'Japanese',
-    }}
+    translation = {
+        'locale': {
+            'id': 'jp',
+            'name': 'Japanese',
+        }
+    }
 
     assert 'jp (Japanese)' == _primary_translation_str(translation)
 
